b253c8884c8f83a2a3012124eb7e697096f81816
Michael Mushrush <mushrushm@gmail.com>
2024-05-25 11:48:15 -0400

    small change

--- amplify/auth/post-confirmation/resource.ts
+++ amplify/auth/post-confirmation/resource.ts
@@ -2,7 +2,4 @@ import { defineFunction } from '@aws-amplify/backend';
 
 export const postConfirmation = defineFunction({
   name: 'post-confirmation',
-  environment: {
-    AMPLIFY_DATA_GRAPHQL_ENDPOINT: 'https://api.example.com/graphql',
-  }
 });
